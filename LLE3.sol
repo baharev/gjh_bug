e42578b3b3793ffc62f68838d8c5b2e74df1f092
baharev <ali.baharev@gmail.com>
2014-09-26 16:20:50 +0200

    Adding files to reproduce bug in Jacobian computation

---
+++ LLE3.sol
@@ -0,0 +1,20 @@
+gjh: "LLE3.gjh" written.  Execute
+ 
+	include "LLE3.gjh"
+	remove "LLE3.gjh";
+
+Options
+9
+0
+2
+0
+7
+20140524
+0
+1
+0
+1
+7
+0
+7
+0
---
+++ LLE3rev.sol
@@ -0,0 +1,20 @@
+gjh: "LLE3rev.gjh" written.  Execute
+ 
+	include "LLE3rev.gjh"
+	remove "LLE3rev.gjh";
+
+Options
+9
+0
+2
+0
+7
+20140524
+0
+1
+0
+1
+7
+0
+7
+0
